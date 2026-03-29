6ba5075737c2cc78e8994e7a47dbdced8919ac73
Musrh <musplay2023@gmail.com>
2026-03-29 22:43:05 +0100

    Refactor UID handling and update template bindings

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { computed } from "vue"
+import Home from "./views/Home.vue"
 
-// 🔥 récupérer UID depuis URL (SANS vue-router)
+// 🔥 récupérer UID depuis URL (hash mode)
 const hash = window.location.hash
 
 let uid = null
@@ -9,24 +11,34 @@ if (hash.includes("uid=")) {
   uid = hash.split("uid=")[1]
 }
 
-console.log("UID =", uid)
+// nettoyer si paramètres après &
+if (uid && uid.includes("&")) {
+  uid = uid.split("&")[0]
+}
 
+const userId = computed(() => uid)
 </script>
 
 <template>
-  <div class="p-6">
+  <div>
 
-    <h1 class="text-2xl font-bold">
-      SaaasGenerator
-    </h1>
+    <!-- HEADER SIMPLE -->
+    <div class="p-4 border-b">
+      <h1 class="text-2xl font-bold">
+        SaaasGenerator
+      </h1>
 
-    <p v-if="uid">
-      UID client : {{ uid }}
-    </p>
+      <p v-if="userId" class="text-green-600">
+        UID client : {{ userId }}
+      </p>
 
-    <p v-else class="text-red-500">
-      Aucun UID reçu
-    </p>
+      <p v-else class="text-red-500">
+        Aucun UID reçu
+      </p>
+    </div>
+
+    <!-- 🔥 HOME COMPONENT -->
+    <Home v-if="userId" :uid="userId" />
 
   </div>
 </template>

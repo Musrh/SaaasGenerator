85d26cafc848561a091016bbe95df2c93a557275
Musrh <musplay2023@gmail.com>
2026-03-29 23:48:29 +0100

    Extract and clean uid from URL hash
    Extract uid from URL hash and clean it before passing to Home component.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 import Home from "./views/Home.vue"
 
 const hash = window.location.hash
+
 let uid = null
 
 if (hash.includes("uid=")) {
   uid = hash.split("uid=")[1]
+
+// 🔥 IMPORTANT: nettoyer params URL
+  uid = uid.split("&")[0]
+  uid = uid.trim()
 }
+
+console.log("🔥 CLEAN UID =", uid)
 </script>
 
 <template>

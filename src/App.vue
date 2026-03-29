01378281b2ce2cd90b053678b6d854847511f3cd
Musrh <musplay2023@gmail.com>
2026-03-29 23:17:21 +0100

    Refactor UID handling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,16 @@
 <script setup>
-import { computed } from "vue"
 import Home from "./views/Home.vue"
 
-// 🔥 récupérer UID depuis URL (hash mode)
 const hash = window.location.hash
-
 let uid = null
 
 if (hash.includes("uid=")) {
   uid = hash.split("uid=")[1]
 }
-
-// nettoyer si paramètres après &
-if (uid && uid.includes("&")) {
-  uid = uid.split("&")[0]
-}
-
-const userId = computed(() => uid)
 </script>
 
 <template>
   <div>
-
-    <!-- HEADER SIMPLE -->
-    <div class="p-4 border-b">
-      <h1 class="text-2xl font-bold">
-        SaaasGenerator
-      </h1>
-
-      <p v-if="userId" class="text-green-600">
-        UID client : {{ userId }}
-      </p>
-
-      <p v-else class="text-red-500">
-        Aucun UID reçu
-      </p>
-    </div>
-
-    <!-- 🔥 HOME COMPONENT -->
-    <Home v-if="userId" :uid="userId" />
-
+    <Home :uid="uid" />
   </div>
 </template>

66dcd0b3429e82bb9998ea660852d08312aed180
Musrh <musplay2023@gmail.com>
2026-04-06 13:33:48 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,3 @@
-<script setup>
-import Home from "./views/Home.vue"
-
-const hash = window.location.hash
-
-let uid = null
-
-if (hash.includes("uid=")) {
-  uid = hash.split("uid=")[1].split("&")[0]
-}
-
-if (uid) {
-  uid = decodeURIComponent(uid).trim()
-}
-
-console.log("🔥 UID FINAL =", uid)
-</script>
-
 <template>
-  <div>
-    <Home :uid="uid" />
-  </div>
+  <router-view />
 </template>

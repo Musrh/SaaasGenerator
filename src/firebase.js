cfa4fa2a8ff129f69cc462c23112461d47590f5f
Musrh <musplay2023@gmail.com>
2026-04-06 01:42:58 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-GbPIVkvxq4Rv1IG2ae6i1Ki8ajui_7k",
+  apiKey: "AIzaSyD-GbPIVkvx4Rv1IG2ae6i1Ki8ajui_7k",
   authDomain: "mrhsaas.firebaseapp.com",
   projectId: "mrhsaas",
   storageBucket: "mrhsaas.firebasestorage.app",
@@ -17,4 +17,9 @@ const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
-export const storage = getStorage(app) // ✅ AJOUT IMPORTANT
+export const storage = getStorage(app)
+
+// 🔥 PERSISTENCE SaaS (IMPORTANT)
+setPersistence(auth, browserLocalPersistence)
+
+export { app }
